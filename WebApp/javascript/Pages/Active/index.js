a9afcf345979429129ba50cce4f0967421bd4ef9
Lite W Kim <litek@orionhealth.com>
2023-04-05 15:55:12 +1200

    allow back navigation form active screen

--- WebApp/javascript/Pages/Active/index.js
+++ WebApp/javascript/Pages/Active/index.js
@@ -1,20 +1,30 @@
 import React from 'react';
 import { SafeAreaView, View, StyleSheet } from 'react-native'
-import { Divider, TopNavigation } from '@ui-kitten/components';
+import { Divider, TopNavigation, TopNavigationAction } from '@ui-kitten/components';
 
 import { BackIcon } from '../../components/Icons';
 
 const Active = ({ route, navigation }) => {
   const { profile } = route.params;
 
+  const navigateBack = () => {
+    navigation.goBack();
+  };
+
   const renderHeader = () => {
     return (
       <TopNavigation
         alignment='center'
         title={profile}
+        subtitle='Active Profile'
+        accessoryLeft={BackAction}
       />
     )
   }
+
+  const BackAction = () => (
+    <TopNavigationAction icon={BackIcon} onPress={navigateBack}/>
+  );
 
   return (
     <SafeAreaView style={{flex: 1}}>
